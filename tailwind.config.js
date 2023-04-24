8cecae566764d40e2c9957f1552852a431f31ccd
Mahdi <kmahdi1997@gmail.com>
2023-04-24 13:09:42 +0430

    header first design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
+    theme: {
+      colors: {
+        seccondColor: '#A55C30',
+      }
+    },
     extend: {},
   },
   plugins: [],
